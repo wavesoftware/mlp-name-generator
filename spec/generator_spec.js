519a9d996a59de07b854fde9f4c8df497b17ab8f
Krzysztof Suszyński <krzysztof.suszynski@coi.gov.pl>
2015-04-02 20:58:17 +0200

    Feature #1: Code changes to generator and random numbers, tests

--- spec/generator_spec.js
+++ spec/generator_spec.js
@@ -1,14 +1,15 @@
 'use strict';
 
 describe("Generator", function () {
+  var generator;
+  var first;
+  beforeEach(function (){
+    var Generator = require('../lib/generator');
+    generator = new Generator();
+    first = generator.generate();
+  });
 
   describe("generate()", function () {
-    var generator;
-    var first;
-    beforeEach(function (){
-      generator = require('../lib/generator');
-      first = generator.generate();
-    });
 
     describe("with default params", function() {
       it("not equal ''", function () {
@@ -30,11 +31,42 @@ describe("Generator", function () {
       it('match /^[A-Z][a-z]+,[A-Z][a-z]+$/', function() {
         var actual;
         var re = /^[A-Z][a-z]+,[A-Z][a-z]+$/;
-        actual = generator.generate('{0},{1}');
+        expect(generator.getFormat()).toEqual('{0}{1}');
+        generator.setFormat('{0},{1}');
+        actual = generator.generate();
         expect(first).not.toMatch(re);
         expect(actual).toMatch(re);
       });
     });
 
+    describe("with seed 'I love you!'", function() {
+      it("returns 'CreamWind'", function() {
+        generator.setSeed('I love you!');
+        expect(generator.generate()).toEqual('CreamWind');
+      });
+    });
+
+    describe("with seed 'I heart you!'", function() {
+      it("returns 'CottonDrop'", function() {
+        generator.setSeed('I heart you!');
+        expect(generator.generate()).toEqual('CottonDrop');
+      });
+    });
+
+
+  });
+  describe('setFormat()', function() {
+    var exceptionText = 'Invalid format secified, format should contain {0} and/or {1} for Pony parts';
+    it('throw error on invalid format', function() {
+      expect( function() { generator.setFormat('Invalid format'); } )
+        .toThrow(new TypeError(exceptionText));
+    });
+    it('uses default format on empty parameter', function() {
+
+      expect( function() { generator.setFormat(); } )
+        .not.toThrow(new TypeError(exceptionText));
+
+      expect(generator.getFormat()).toEqual('{0}{1}');
+    });
   });
 });
